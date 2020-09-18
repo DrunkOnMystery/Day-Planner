8296168fb933ab1a299056c1796e61f612d764d3
Ben Brumby <bbrumby00@gmail.com>
2020-09-17 22:33:29 -0400

    css is partially done. Working on JS variables

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //Establish variables
 var displayDateEl = document.querySelector("#currentDay");
-var 
+var currentHourEl = document.querySelector(".past");
+var currentHour = moment().hour();
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var button = $("<button>");
+var icon = $("<i>");
 
 // Display the current time using Moment.js
 console.log(moment().format("dddd, MMMM Do"));
@@ -9,6 +13,8 @@ console.log(moment().format("dddd, MMMM Do"));
 
 // When app opens, display content for each time block from local storage.
         //it can be done with a loop(more efficiently) or by targeting each time block separately
+    for (i = 9; i < 18; i++)
+    if (hours[i] < currentHour)
 
     //Display the current date
     displayDateEl.textContent = moment().format("dddd, MMMM Do");
